5c96c2a660d74ce38807fce8f050f1adb05d00ff
Malcolm <malcolmjmr@gmail.com>
2015-06-13 23:13:12 -0400

    small edit to spacing

--- intro_to_ruby_and_web_dev/lesson_one/tic_tac_toe.rb
+++ intro_to_ruby_and_web_dev/lesson_one/tic_tac_toe.rb
@@ -1,8 +1,8 @@
 require 'pry'
 # display functions
 def welcome_user 
-	puts 'Welcome to Tic Tac Toe. Press any enter to start'
-	gets
+  puts 'Welcome to Tic Tac Toe. Press any enter to start'
+  gets
 end 
 
 def display_board(b)
@@ -14,57 +14,56 @@ def display_board(b)
 end
 
 def display_winner(b)
-	if b[:winner] = 'X'
-		puts 'You win!'
-	elsif b[:winner] = 'O'
-		puts 'You lose'
-	else 
-		puts 'There was a tie.'
-	end
+  if b[:winner] = 'X'
+    puts 'You win!'
+  elsif b[:winner] = 'O'
+    puts 'You lose'
+  else 
+    puts 'There was a tie.'
+  end
 end
 
 # state functions
 def initialize_board
-	b = {}
-	(1..9).each {|pos| b[pos] = pos}
-	b[:winning_lines] = [
-		[1,2,3],
-		[4,5,6],
-		[7,8,9],
-		[1,5,9],
-		[7,5,9],
-		[1,4,7],
-		[2,5,8],
-		[3,6,9]
-	]
-	b[:x_pos] = []
-	b[:o_pos] = []
-	b[:empty_pos] = [1,2,3,4,5,6,7,8,9]
-	b
+  b = {}
+  (1..9).each {|pos| b[pos] = pos}
+  b[:winning_lines] = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,5,9],
+    [7,5,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9]
+  ]
+  b[:x_pos] = []
+  b[:o_pos] = []
+  b[:empty_pos] = [1,2,3,4,5,6,7,8,9]
+b
 end
 
 def add_move(b, pos, player_symbol)
   b[pos] = player_symbol
   b[:empty_pos].delete(pos)
   if player_symbol == 'X'
-  	b[:x_pos] << pos
+    b[:x_pos] << pos
   elsif player_symbol == 'O'
-  	b[:o_pos] << pos
+    b[:o_pos] << pos
   end
 end
 
 def there_is_winner(b)
-
-	b[:winning_lines].each do |line|
-		if get_overlap(b[:x_pos], line).length == 3
-			b[:winner] = 'X'
-			return true
+  b[:winning_lines].each do |line|
+    puts get_overlap(b[:o_pos], line)
+    if get_overlap(b[:x_pos], line).length == 3
+      b[:winner] = 'X'
+      return true
     elsif get_overlap(b[:o_pos], line).length == 3
-    	b[:winner] = 'O'
-
-    	return true
+      b[:winner] = 'O'
+      return true
     else
-    	return false
+      return false
     end
   end
 end
@@ -77,50 +76,50 @@ end
 def get_user_move(b)
   pos_is_available = false
   begin 
-		puts "Pick a square (1 - 9):"
-		pos = gets.chomp.to_i
-		if pos < 10 && b[:empty_pos].include?(pos) then pos_is_available = true end
-	end until pos_is_available
+    puts "Pick a square (1 - 9):"
+    pos = gets.chomp.to_i
+    if pos < 10 && b[:empty_pos].include?(pos) then pos_is_available = true end
+  end until pos_is_available
   add_move(b, pos, 'X') 
 end
 
 # computer moves functions 
 def get_comp_move(b)
-	possible_moves = []
-	if b[:o_pos].length == 0 
-		possible_moves[0] = b[:empty_pos].sample
-	else 
-  	b[:winning_lines].each do |line|
-  	  comp_squares = get_overlap(line, b[:o_pos])
-  	  empty_squares = get_overlap(line, b[:empty_pos])
-  	  user_squares = get_overlap(line, b[:x_pos])
+  possible_moves = []
+  if b[:o_pos].length == 0 
+    possible_moves[0] = b[:empty_pos].sample
+  else 
+    b[:winning_lines].each do |line|
+      comp_squares = get_overlap(line, b[:o_pos])
+      empty_squares = get_overlap(line, b[:empty_pos])
+      user_squares = get_overlap(line, b[:x_pos])
       if empty_squares.length < 3
-      	# win
-      	if comp_squares.length == 2 
-  	  		possible_moves[0] = empty_squares.sample
-  	  	# stop user from winning
-  	  	elsif user_squares.length == 2	
-  	  		possible_moves[1] = empty_squares.sample
-  	  	# second square in line
-  	  	elsif comp_squares.length == 1 && user_squares.length == 0
-  	  		possible_moves[2] = empty_squares.sample
-  	  	# block line for user
-  	  	elsif comp_squares.length == 0 && user_squares.length == 1
-					possible_moves[3] = empty_squares.sample
-				# place first square  
-				elsif empty_squares.length == 3
-					possible_moves[4] = empty_squares.sample
-				end
-			end
-		end
-	end
-	possible_moves.each {|move| return add_move(b, move, 'O') if move.class == Fixnum}
+        # win
+        if comp_squares.length == 2 
+          possible_moves[0] = empty_squares.sample
+        # stop user from winning
+        elsif user_squares.length == 2	
+          possible_moves[1] = empty_squares.sample
+        # second square in line
+        elsif comp_squares.length == 1 && user_squares.length == 0
+          possible_moves[2] = empty_squares.sample
+        # block line for user
+        elsif comp_squares.length == 0 && user_squares.length == 1
+          possible_moves[3] = empty_squares.sample
+        # place first square  
+        elsif empty_squares.length == 3
+          possible_moves[4] = empty_squares.sample
+        end
+      end
+    end
+  end
+  possible_moves.each {|move| return add_move(b, move, 'O') if move.class == Fixnum}
 end
 
 
 def get_overlap(arr1, arr2)
-	concat = (arr1 + arr2).flatten
-	overlap = concat.select{|e| concat.count(e) > 1}.uniq
+  concat = (arr1 + arr2).flatten
+  overlap = concat.select{|e| concat.count(e) > 1}.uniq
 end
 
 
@@ -129,12 +128,12 @@ welcome_user
 board = initialize_board
 display_board(board)
 loop do
-	get_user_move(board) 
-	break if there_is_winner(board) || game_ended(board)
-	display_board(board)
-	get_comp_move(board)
-	break if there_is_winner(board) || game_ended(board)
-	display_board(board)
+  get_user_move(board) 
+  break if there_is_winner(board) || game_ended(board)
+  display_board(board)
+  get_comp_move(board)
+  break if there_is_winner(board) || game_ended(board)
+  display_board(board)
 end
 display_winner(board)
 
